a430cabccfd0b82d9893a26e568614156fd50ae4
hadirkhan <hadir.khan@outlook.com>
2016-10-21 04:26:24 -0500

    adds path module to support file paths in WIndoYs and Linux

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var path = require('path');
+var require 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.sendFile('home.html', {root: __dirname + '\\..\\'+ 'views'});
+  res.sendFile('home.html', {root: path.join(__dirname, 'views')});
 });
 
 module.exports = router;
