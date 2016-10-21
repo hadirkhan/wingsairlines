7898eff079ed8fe86b4604cf52623421cfa584d7
hadirkhan <hadir.khan@outlook.com>
2016-10-21 04:30:10 -0500

    fixes error in file path

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.sendFile('home.html', {root: path.join(__dirname, 'views')});
+  res.sendFile('home.html', {root: path.join(__dirname, '..', 'views')});
 });
 
 module.exports = router;
