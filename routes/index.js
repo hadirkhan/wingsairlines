6b588bdebac120d239476487d8a97465f71ef6fb
hadirkhan <hadir.khan@outlook.com>
2016-10-25 18:54:34 -0500

    Homepage rework with Jade and Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var require 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.sendFile('home.html', {root: path.join(__dirname, '..', 'views')});
+  res.render('index', { title: 'Wings-Welcome!' });
+  //res.sendFile('home.html', {root: path.join(__dirname, '..', 'views')});
 });
 
 module.exports = router;
