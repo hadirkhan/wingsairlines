ef04b79e4493292606ed79612b6104bad3943f36
hadirkhan <hadir.khan@outlook.com>
2016-10-26 17:53:41 -0500

    Integrates prototype demo work

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,20 @@ router.get('/', function(req, res, next) {
   //res.sendFile('home.html', {root: path.join(__dirname, '..', 'views')});
 });
 
+router.get('/search', function(req, res, next){
+  res.sendFile('search.html', {root: path.join(__dirname, '..', 'views')});
+});
+
+router.get('/payments', function(req, res, next){
+  res.sendFile('payments.html', {root: path.join(__dirname, '..', 'views')});
+});
+
+router.get('/confirmation', function(req, res, next){
+  res.sendFile('confirmation.html', {root: path.join(__dirname, '..', 'views')});
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', {title: 'About Us'});
+});
+
 module.exports = router;
