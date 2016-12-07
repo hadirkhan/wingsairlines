32d085b4e8af8b8308f9a30971678cec44161db5
Arushee Garg <Arushee@Arushees-MacBook-Pro.local>
2016-12-07 16:34:21 -0600

    cancel value check and incomplete bookingsModel

--- models/BookingsModel.js
+++ models/BookingsModel.js
@@ -8,28 +8,29 @@ function cancelFlights(pnr,callback)
     const conn = dbutil.getConnection();
 
     return new Promise(function (resolve, reject) {
-        const cancelQuery = "UPDATE reservation SET reservation_status = 'Cancelled' where passenger_pnr = ?;";
-        const getticketQuery = "Select ticket_no from reservation where passenger_pnr = ?;";
+        const cancelQuery = "UPDATE reservation SET reservation_status = 'Cancelled' where reservation_status not like 'Cancelled' AND passenger_pnr = ?;";
 
-        dbutil.getConnection().query(cancelQuery, [pnr], function(err, rows, fields){
-            if(!err) {
-                resolve(rows);
-                dbutil.getConnection().query(getticketQuery, [pnr], function(err, rows, fields){
-                    if(!err){
-                        conn.query("Delete from ticket where ticket_no = ?", [ticket_no], function(err, rows, fields) {
-                    }
-                allPromises = reservations.map(function(passenger_pnr, index) {
-                  return new Promise(function(resolve, reject){
+        // const getticketQuery = "Select ticket_no from reservation where passenger_pnr = ?;";
+
+        // dbutil.getConnection().query(cancelQuery, [pnr], function(err, rows, fields){
+        //     if(!err) {
+        //         resolve(rows);
+        //         dbutil.getConnection().query(getticketQuery, [pnr], function(err, rows, fields){
+        //             if(!err){
+        //                 conn.query("Delete from ticket where ticket_no = ?", [ticket_no], function(err, rows, fields) {
+        //             }
+        //         allPromises = reservations.map(function(passenger_pnr, index) {
+        //           return new Promise(function(resolve, reject){
                     
-                        if(err){
-                            reject(err, 'Error occurred in canceling ticket for pnr' + reservation.passenger_pnr);
-                        } else if(!err){
-                            results[reservation_status] = 'Cancelled';
-                            resolve();
-                        }
-                    });
-                  });
-                });  
+        //                 if(err){
+        //                     reject(err, 'Error occurred in canceling ticket for pnr' + reservation.passenger_pnr);
+        //                 } else if(!err){
+        //                     results[reservation_status] = 'Cancelled';
+        //                     resolve();
+        //                 }
+        //             });
+        //           });
+        //         });  
 
                 Promise.all(allPromises)
                 .then(function(){
